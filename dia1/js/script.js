b56defa326b92c03db0350dce1816c41a51db6f1
AinhoaPh <ainhoiitta.phernandez@alumnos.cei.es>
2025-04-23 20:22:21 +0200

    dia 2

--- dia1/js/script.js
+++ dia1/js/script.js
@@ -2,10 +2,6 @@ const btn = document.getElementById("btn");
 const curiosidad = document.querySelector(".curiosidad");
 
 btn.addEventListener("click", () => { 
-    if {
-        curiosidad.classList.add(".active");
-     
-    }else{
-        curiosidad.classList.remove(".active");
-    }
+    curiosidad.classList.toggle(".active");
+
  })
