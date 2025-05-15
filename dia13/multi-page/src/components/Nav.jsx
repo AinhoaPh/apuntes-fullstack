b2994c12e1ba098dd6ca0f1facd4166b9e31a3b1
AinhoaPh <ainhoiitta.phernandez@alumnos.cei.es>
2025-05-15 21:59:21 +0200

    dia 13

--- dia13/multi-page/src/components/Nav.jsx
+++ dia13/multi-page/src/components/Nav.jsx
@@ -1,7 +1,7 @@
 export const NavSearchParams = () => {
     return ( 
         <>
-        <ul>
+        <ul className="flex gap-5 ">
             <li><a href="?page=home">Home</a></li>
             <li><a href="?page=contact">Contacto</a></li>
             <li><a href="?page=about">Nosotros</a></li>
